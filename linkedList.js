17022ff1f1163225b4b6b6d32499db0180e13c22
Tu Vu <vuanhtu52@gmail.com>
2023-10-24 15:44:10 +0700

    Add prepend function for LinkedList

--- linkedList.js
+++ linkedList.js
@@ -43,6 +43,19 @@ const LinkedList = () => {
         }
     };
 
+    const prepend = value => {
+        // Special case when the list is empty
+        if (head === null && tail === null) {
+            let node = Node(value, null);
+            head = node;
+            tail = node;
+        } else {
+            let node = Node(value, null);
+            node.setNextNode(head);
+            head = node;
+        }
+    };
+
     const getHead = () => {
         return head;
     };
@@ -52,16 +65,23 @@ const LinkedList = () => {
     };
 
     return {
-        append,
         getHead,
         getTail,
+        append,
+        prepend,
     }
 };
 
 let list = LinkedList();
-list.append("3");
-list.append("6");
-list.append("9");
-list.append("12");
+// list.append("3");
+// list.append("6");
+// list.append("9");
+// list.append("12");
+
+list.prepend("3");
+list.prepend("6");
+list.prepend("9");
+list.prepend("12");
+
 console.log(list.getHead().getValue());
 console.log(list.getTail().getValue());
